172ca4eaf6b180df0c7fa44b769a3370ccac5b6b
Christian Blades <christian.blades+github@gmail.com>
2018-02-08 21:04:00 -0500

    ipv6, quickchecks

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,15 @@ use std::fmt::{Display, Formatter};
 use std::fmt;
 use std::ops::{ShlAssign, AddAssign};
 
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
 #[derive(Debug,PartialEq)]
 pub enum QuintError {
     InputTooSmall,
     InputTooLarge,
+    InputInvalid,
 }
 
 impl Display for QuintError {
@@ -34,6 +39,7 @@ impl Display for QuintError {
         let out = match *self {
             QuintError::InputTooLarge => "proquint was too large",
             QuintError::InputTooSmall => "expected larger proquint",
+            QuintError::InputInvalid => "input was not a valid proquint",
         };
         write!(f, "{}", out)
     }
@@ -44,6 +50,7 @@ impl error::Error for QuintError {
         match *self {
             QuintError::InputTooLarge => "proquint was too large",
             QuintError::InputTooSmall => "expected larger proquint",
+            QuintError::InputInvalid => "input was not a valid proquint",
         }
     }
 
@@ -163,6 +170,58 @@ pub fn from_quint<T>(quint: &str) -> (T, usize)
     (res, bitcounter)
 }
 
+pub fn unquint_exactly<T>(quint: &str, bits: usize) -> Result<(T, usize), QuintError>
+    where T: Sized + Default + ShlAssign<isize> + AddAssign<T> + From<u8>
+{
+    let mut bitcounter = 0usize;
+    let mut res: T = T::default();
+    let mut final_idx = 0usize;
+    for (i, c) in quint.chars().enumerate() {
+        match c {
+            // consonants
+            'b' => decons!(res, bitcounter, T::from(0u8)),
+            'd' => decons!(res, bitcounter, T::from(1u8)),
+            'f' => decons!(res, bitcounter, T::from(2u8)),
+            'g' => decons!(res, bitcounter, T::from(3u8)),
+            'h' => decons!(res, bitcounter, T::from(4u8)),
+            'j' => decons!(res, bitcounter, T::from(5u8)),
+            'k' => decons!(res, bitcounter, T::from(6u8)),
+            'l' => decons!(res, bitcounter, T::from(7u8)),
+            'm' => decons!(res, bitcounter, T::from(8u8)),
+            'n' => decons!(res, bitcounter, T::from(9u8)),
+            'p' => decons!(res, bitcounter, T::from(10u8)),
+            'r' => decons!(res, bitcounter, T::from(11u8)),
+            's' => decons!(res, bitcounter, T::from(12u8)),
+            't' => decons!(res, bitcounter, T::from(13u8)),
+            'v' => decons!(res, bitcounter, T::from(14u8)),
+            'z' => decons!(res, bitcounter, T::from(15u8)),
+
+            // vowels
+            'a' => devowel!(res, bitcounter, T::from(0u8)),
+            'i' => devowel!(res, bitcounter, T::from(1u8)),
+            'o' => devowel!(res, bitcounter, T::from(2u8)),
+            'u' => devowel!(res, bitcounter, T::from(3u8)),
+
+            // separators
+            _ => (),
+        }
+        if bitcounter >= bits {
+            final_idx = i;
+            break;
+        }
+    }
+
+    if bitcounter == bits {
+        return Ok((res, final_idx));
+    }
+
+    if bitcounter >  bits {
+        Err(QuintError::InputInvalid)
+    } else {
+        Err(QuintError::InputTooSmall)
+    }
+}
+
 macro_rules! impl_from_quint {
     ($expected_bits:expr) => {
         fn from_quint(quint: &str) -> Result<Self, QuintError> {
@@ -257,18 +316,40 @@ impl Quintable for std::net::Ipv4Addr {
     }
 }
 
+impl Quintable for std::net::Ipv6Addr {
+    fn to_quint(&self) -> String {
+        let segments: [u16; 8] = self.segments();
+        let out: Vec<String> = segments.iter().map(|s| s.to_quint()).collect();
+        out.join("-")
+    }
+
+    fn from_quint(quint: &str) -> Result<Self, QuintError> {
+        let q = &quint;
+        let (first, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (second, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (third, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (fourth, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (fifth, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (sixth, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (seventh, last_i) = unquint_exactly(q, 16)?;
+        let q = &q[last_i+1..];
+        let (eighth, _) = unquint_exactly(q, 16)?;
+
+        Ok(std::net::Ipv6Addr::new(first, second, third, fourth, fifth, sixth, seventh, eighth))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::net::Ipv4Addr;
+    use std::net::{Ipv4Addr, Ipv6Addr};
     use Quintable;
     use QuintError;
-
-    fn ipv4_test(ipv4: [u8; 4], quint: &str) {
-        assert_eq!(Ipv4Addr::from(ipv4).to_quint(), quint);
-
-        let actual = Ipv4Addr::from_quint(quint).unwrap();
-        assert_eq!(actual, Ipv4Addr::from(ipv4));
-    }
 
     #[test]
     fn quint_too_small() {
@@ -282,6 +363,13 @@ mod tests {
         assert_eq!(u16::from_quint("lubab-gutuz").err(), Some(QuintError::InputTooLarge));
         assert_eq!(u32::from_quint("lubab-gutuz-kobim").err(), Some(QuintError::InputTooLarge));
         assert_eq!(u64::from_quint("lubab-gutuz-kobim-fival-bison").err(), Some(QuintError::InputTooLarge));
+    }
+
+    fn ipv4_test(ipv4: [u8; 4], quint: &str) {
+        assert_eq!(Ipv4Addr::from(ipv4).to_quint(), quint);
+
+        let actual = Ipv4Addr::from_quint(quint).unwrap();
+        assert_eq!(actual, Ipv4Addr::from(ipv4));
     }
 
     #[test]
@@ -311,4 +399,49 @@ mod tests {
         // 12.110.110.204  budov-kuras
         ipv4_test([12, 110, 110, 204], "budov-kuras");
     }
+
+    fn back_and_forth<T>(xs: T) -> bool
+        where T: Quintable + PartialEq
+    {
+        let quint = xs.to_quint();
+        let y = match T::from_quint(&quint) {
+            Ok(res) => res,
+            Err(e) => {
+                println!("error! {:?}", e);
+                return false;
+            },
+        };
+        xs == y
+    }
+
+    quickcheck! {
+        fn u16(xs: u16) -> bool {
+            back_and_forth(xs)
+        }
+    }
+
+    quickcheck! {
+        fn u32(xs: u32) -> bool {
+            back_and_forth(xs)
+        }
+    }
+
+    quickcheck! {
+        fn u64(xs: u64) -> bool {
+            back_and_forth(xs)
+        }
+    }
+
+    quickcheck! {
+        fn ipv4(xs: Ipv4Addr) -> bool {
+            back_and_forth(xs)
+        }
+    }
+
+    quickcheck! {
+        fn ipv6(xs: Ipv6Addr) -> bool {
+            back_and_forth(xs)
+        }
+    }
+
 }
